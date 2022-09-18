40354743b826a04c963dae53067f103a012a13cc
GeraLZ <gera.lz2305@gmail.com>
2022-09-18 12:32:06 -0500

    Trabajando secciones y agregando estilos

---
+++ C11_300822_MercadoLiebre/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const { clearScreenDown } = require('readline');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3030,() =>{
+    console.log('Servidor corriendo en el 3030')
+})
+
+app.get('/',(req,res) => {
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname,'src/views/home.html'))
+    //res.sendFile(path.resolve('./views/index.html'))
+} )
